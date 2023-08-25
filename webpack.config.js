b4502222678c5d9baf3a354b630cdcf4c85e208d
PavliukD <albatrozzz1@gmail.com>
2023-08-26 01:08:21 +0200

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require('path');
 
-let htmlPageNames = [];
+let htmlPageNames = ['404'];
 let multipleHtmlPlugins = htmlPageNames.map(name => {
   return new HtmlWebpackPlugin({
     template: `./src/${name}.html`, // relative path to the HTML files
